f1a7ffbb2c1aa71c64c8abfe89eabc47cc59aca1
esam <emmanueldaud358@gmail.com>
2026-05-17 17:18:34 +0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+process.env.NODE_ENV !== 'production' && require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
